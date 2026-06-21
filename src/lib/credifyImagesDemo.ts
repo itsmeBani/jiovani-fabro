208d7b631d9435663273f87a38c82ac1e24c63ad
fabro <fabrojiovani1@gmail.com>
2026-06-21 13:27:33 +0800

    added demo images fopr credify app

--- src/lib/credifyImagesDemo.ts
+++ src/lib/credifyImagesDemo.ts
@@ -7,10 +7,13 @@ import six from "../assets/demo/credify/6.webp";
 import seven from "../assets/demo/credify/7.webp";
 import eight from "../assets/demo/credify/8.webp";
 import nine from "../assets/demo/credify/9.webp";
+import nine_1 from "../assets/demo/credify/9.1.webp";
 import ten from "../assets/demo/credify/10.webp";
 import eleven from "../assets/demo/credify/11.webp";
 import twelve from "../assets/demo/credify/12.webp";
 import thirteen from "../assets/demo/credify/13.webp";
+import fortheen from "../assets/demo/credify/14.webp";
+import fifteen from "../assets/demo/credify/15.webp";
 
 export const CredifyImagesDemo = [
     one,
@@ -21,9 +24,12 @@ export const CredifyImagesDemo = [
     six,
     seven,
     eight,
-    nine,
-    ten,
+  nine_1,
+
+    ten, nine,
     eleven,
     twelve,
     thirteen,
+  fortheen,
+  fifteen
 ];
